8c9f1676222bd14ddc421c69c617e3c126bbe95c
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2019-06-25 08:56:36 +0800

    Correct formatting - Message should be 'Hello, <user>!" - Add Null check

--- samples/greeter/src/main/java/functions/Greeter.java
+++ samples/greeter/src/main/java/functions/Greeter.java
@@ -5,6 +5,6 @@ import java.util.function.Function;
 public class Greeter implements Function<String, String> {
 
 	public String apply(String name) {
-		return "Hello " + name;
+		return String.format("Hello, %s!", name == null || name.isBlank() ? "World" : name);
 	}
 }
